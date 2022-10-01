00e8d8af212292edfc52103dd4fce26988a1676c
Taylor Guenthner <wtguenthner@gmail.com>
2022-10-01 13:04:45 -0400

    Added component folders to client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import Battle from './components/Battle';
+import Cards from './components/Cards';
+import Login from './components/Login';
+import Menu from './components/Menu';
+import Profile from './components/Profile';
+import Shop from './components/Shop';
+
+const [page, setPage] = useState('');
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
