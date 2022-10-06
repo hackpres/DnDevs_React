34f2e1e57a25748c3c404388c4cb81f67d81a04e
josh.moran501 <josh.moran501@gmail.com>
2022-10-06 11:53:34 -0400

    built out navbar

--- client/src/components/Navigation/Nav.jsx
+++ client/src/components/Navigation/Nav.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import NavIcon from './NavIcon'
+import NavScroll from './NavScroll'
+import NavText from './NavText'
+import Navigation from '../Buttons/Navigation'
+
+// shop home code profile
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <>
+      <Navigation title="Shop" destination="shop">
+        <NavIcon />
+        <NavScroll />
+        <NavText/>
+      </Navigation>
+
+      <Navigation title="Home" destination="home">
+        <NavIcon />
+        <NavScroll />
+        <NavText/>
+      </Navigation>
+
+      <Navigation title="Code" destination="code">
+          <NavIcon />
+          <NavScroll />
+          <NavText/>
+      </Navigation>
+
+      <Navigation title="Profile" destination="profile">
+        <NavIcon />
+        <NavScroll />
+        <NavText/>
+      </Navigation>
+    </>
   )
 }
 
