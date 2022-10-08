4f5fe760deee86132e56ca4e052dd28b64653d83
Taylor Guenthner <wtguenthner@gmail.com>
2022-10-08 13:06:35 -0400

    Added find one User mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,75 +1,74 @@
-const { Bosses, User } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { Bosses, User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await user
+          .findOne({
+            _id: context.user_id,
+          })
+          .select("-__v -password");
 
-        Query:{
-            me: async (parent, args,context) =>{
-                if(context.user){
-                    const userData = await user.findOne({
-                        _id: context.user_id
-                    }).select("-__v -password");
-    
-                    return userData;
-                }
-                throw new AuthenticationError("Not Logged In");
-            },
-            bosses: async () => {
-                return Bosses.find();
-              },
-              users: async()=>{
-                return User.find();
-              }
+        return userData;
+      }
+      throw new AuthenticationError("Not Logged In");
+    },
+    bosses: async () => {
+      return Bosses.find();
+    },
+    users: async () => {
+      return User.find();
+    },
+    user: async (parent, {userId}) =>{
+     return User.findOne(
+         {_id: userId}
+      );
+     
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        throw new AuthenticationError("No profile with this username found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addCard: async (parent, { userId, card }) => {
+      console.log(card);
+      // return;
+      return User.findOneAndUpdate(
+        { _id: userId },
+        {
+          $push: { savedCards: card },
         },
-
-        Mutation: {
-            addUser: async (parent,args)=>{
-                const user = await User.create(args);
-                const token = signToken(user);
-                return {token, user}
-
-            },
-            login: async (parent, { username, password }) => {
-                const user = await User.findOne({ username});
-          
-                if (!user) {
-                  throw new AuthenticationError('No profile with this username found!');
-                }
-          
-                const correctPw = await user.isCorrectPassword(password);
-          
-                if (!correctPw) {
-                  throw new AuthenticationError('Incorrect password!');
-                }
-          
-                const token = signToken(user);
-                return { token, user };
-              },
-              addCard: async (parent, {userId, card})=>{
-                console.log(card);
-                // return;
-                return User.findOneAndUpdate(
-                  {_id: userId},
-                  {
-                    $push: {savedCards: card}
-                  },
-                  {
-                    new: true,
-                    runValidators: true,
-                  }
-                )
-              },
-              removeUser: async (parent, {userId}) =>{
-                return User.findOneAndDelete({_id: userId});
-              },
+        {
+          new: true,
+          runValidators: true,
         }
-
-
-
-
+      );
+    },
+    removeUser: async (parent, { userId }) => {
+      return User.findOneAndDelete({ _id: userId });
+    },
+  },
 };
-
-
-
 
 module.exports = resolvers;
