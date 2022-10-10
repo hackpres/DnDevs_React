325ec505d1cdf2690edc8b95da2c590715d35bcb
Taylor Guenthner <wtguenthner@gmail.com>
2022-10-10 19:26:25 -0400

    Added signup function on client side

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "../components/Headings/Heading";
 import { Formik, Form } from "formik";
 import Input from "../components/Template/Input";
@@ -8,10 +8,45 @@ import SupportModalContent from "../components/Modal/SupportModalContent";
 import signupSchema from "../utils/signupSchema";
 import { Link } from "react-router-dom";
 
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+
 function Signup() {
-  const handleSubmit = (values) => {
-    console.log(values);
+  const [formState, setFormState] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
+  const handleChange = (event) => {
+    const { username, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [username]: value,
+    });
   };
+
+  const handleSignUpSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addUser({
+        variabloes: { ...formState },
+      });
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // const handleSubmit = (values) => {
+  //   console.log(values);
+  // };
+
   return (
     <>
       <Heading title="Signup" />
@@ -21,18 +56,26 @@ function Signup() {
           password: "",
         }}
         validationSchema={signupSchema}
-        onSubmit={handleSubmit}
+        onSubmit={handleSignUpSubmit}
       >
         {({ values, errors, touched }) => (
           <Form>
-            <Input title="username" value={values.username}></Input>
+            <Input
+              title="username"
+              type="text"
+              value={formState.username}
+              placeholder="Your Username"
+              onChange={handleChange}
+            ></Input>
             {errors.username && touched.username
               ? console.log(errors.username)
               : null}
             <Input
               title="password"
               type="password"
-              value={values.password}
+              placeholder="******"
+              value={formState.password}
+              onChange={handleChange}
             ></Input>
             {errors.password && touched.password
               ? console.log(errors.password)
