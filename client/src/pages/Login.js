9d02e9e4818807c758a1080b7412f0724d25ef48
josh.moran501 <josh.moran501@gmail.com>
2022-10-10 18:39:02 -0400

    input component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,24 +3,45 @@ import Heading from "../components/Headings/Heading";
 import { Formik, Form } from "formik";
 import Input from "../components/Template/Input";
 import Navigation from "../components/Buttons/Navigation";
-import Modals from '../components/Modal/Modals';
-import SupportModalContent from '../components/Modal/SupportModalContent';
-
+import Modals from "../components/Modal/Modals";
+import SupportModalContent from "../components/Modal/SupportModalContent";
+import signupSchema from "../utils/signupSchema";
+import { Link } from "react-router-dom";
 
 function Login() {
-  const validateUsername = () => {};
-  const validatePassword = () => {};
-
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
   return (
     <>
-      <Heading h="h2" title="Login" />
-      <Formik>
-        <Form>
-          <Input title="username" validate={validateUsername}></Input>
-          <Input title="password" validate={validatePassword}></Input>
-        </Form>
+      <Formik
+        initialValues={{
+          username: "",
+          password: "",
+        }}
+        validationSchema={signupSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ values, errors, touched }) => (
+          <Form>
+            <Input title="username" value={values.username}></Input>
+            {errors.username && touched.username
+              ? console.log(errors.username)
+              : null}
+            <Input
+              title="password"
+              type="password"
+              value={values.password}
+            ></Input>
+            {errors.password && touched.password
+              ? console.log(errors.password)
+              : null}
+            <Link to="/home">
+              <button type="submit">Submit</button>
+            </Link>
+          </Form>
+        )}
       </Formik>
-      <Navigation title="Submit" destination="home" />
       <Modals label="Support" modalContent={<SupportModalContent />} />
     </>
   );
