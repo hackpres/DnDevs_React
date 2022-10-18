947e932bb8c441f77a2a31e7e04c4ae3369ac827
hackpres <102390515+hackpres@users.noreply.github.com>
2022-10-18 11:54:06 -0400

    removed provider from index.js and created one for App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
+import RootLayout from "./pages/RootLayout";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
-import Battle from "./pages/Battle";
+import Battle, { Loader as cardLoader } from "./pages/Battle";
 import Code from "./pages/Snippets";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -8,28 +9,23 @@ import Menu from "./pages/Home";
 import Profile from "./pages/Profile";
 import Shop from "./pages/Shop";
 import Creation from "./pages/CharacterCreation";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
 import { createGlobalStyle } from "styled-components";
-import { setContext } from "@apollo/client/link/context";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { setContext } from '@apollo/client/link/context';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("id_token");
+  const token = localStorage.getItem('id_token');
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
+      authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
@@ -44,27 +40,31 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     width: 100%;
-    height:100%;
     overflow-x: hidden;
   }
 `;
 
+const router = createBrowserRouter(createRoutesFromElements(      
+  <Route path="/" element={<RootLayout />}>
+    <Route index element={<Landing />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/shop" element={<Shop />} />
+    <Route path="/menu" element={<Menu />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/code" element={<Code />} />
+    <Route path="/battle" element={<Battle />} loader={cardLoader} />
+    <Route path="/creation" element={<Creation />} />
+  </Route>
+));
+
 function App() {
+
   return (
     <ApolloProvider client={client}>
       <GlobalStyle />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/home" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/shop" element={<Shop />} />
-        <Route exact path="/menu" element={<Menu />} />
-        <Route exact path="/profile" element={<Profile />} />
-        <Route exact path="/code" element={<Code />} />
-        <Route exact path="/battle" element={<Battle />} />
-        <Route exact path="/creation" element={<Creation />} />
-      </Routes>
+      <RouterProvider router={router} />
     </ApolloProvider>
   );
 }
