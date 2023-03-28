aadfa20d4a2011cbdb2b3e37a025e80de8641ce6
hackpres <102390515+hackpres@users.noreply.github.com>
2023-03-28 10:22:25 -0400

    removed createHttpLink from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,24 +14,25 @@ import { createGlobalStyle } from "styled-components";
 import { setContext } from '@apollo/client/link/context';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
 
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('id_token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  // link: authLink.concat(httpLink),
+  uri: `/graphql`,
   cache: new InMemoryCache(),
 });
 
