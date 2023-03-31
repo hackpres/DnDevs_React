ab044c51de312522dbbd45dde69869413c159c00
hackpres <102390515+hackpres@users.noreply.github.com>
2023-03-31 09:20:11 -0400

    fixed seed.js import of require for .json requires

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,35 +3,34 @@ import jwt from "jsonwebtoken";
 const secret = "mysecretssshhhhhhh";
 const expiration = "2h";
 
-// const authMiddleware = function ({ req }) {
-//   // allows token to be sent via req.body, or headers
-//   let token = req.body.token || req.headers.authorization;
+const authMiddleware = function ({ req }) {
+  // allows token to be sent via req.body, or headers
+  let token = req.body.token || req.headers.authorization;
 
-//   // We split the token string into an array and return actual token
-//   if (req.headers.authorization) {
-//     token = token.split(" ").pop().trim();
-//   }
-
-//   if (!token) {
-//     return req;
-//   }
-
-//   // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
-//   try {
-//     const { data } = jwt.verify(token, secret, { maxAge: expiration });
-//     req.user = data;
-//   } catch {
-//     console.log("Invalid token");
-//   }
-
-//   // return the request object so it can be passed to the resolver as `context`
-//   return req;
-// }
-
-module.exports = {
-  signToken: function ({ username, _id }) {
-    const payload = { username, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  // We split the token string into an array and return actual token
+  if (req.headers.authorization) {
+    token = token.split(" ").pop().trim();
   }
+
+  if (!token) {
+    return req;
+  }
+
+  // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+  } catch {
+    console.log("Invalid token");
+  }
+
+  // return the request object so it can be passed to the resolver as `context`
+  return req;
 }
-// export { authMiddleware, signToken };
+
+const signToken = function ({ username, _id }) {
+  const payload = { username, _id };
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+}
+
+export { authMiddleware, signToken };
