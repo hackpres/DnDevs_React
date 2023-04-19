6c4e4cc3724848d65c0a6dee816795e701fe8869
hackpres <102390515+hackpres@users.noreply.github.com>
2023-04-19 12:29:57 -0400

    moved ApolloClient context from App.js to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,34 +9,9 @@ import Menu from "./pages/Home";
 import Profile from "./pages/Profile";
 import Shop from "./pages/Shop";
 // import Creation from "./pages/CharacterCreation";
-import { ApolloClient, InMemoryCache, ApolloProvider, concat, ApolloLink, HttpLink } from "@apollo/client";
 import { createGlobalStyle } from "styled-components";
-// import { setContext } from '@apollo/client/link/context';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
-const httpLink = new HttpLink({
-  uri: '/graphql',
-});
-
-const authLink = new ApolloLink((operation, forward) => {
-  operation.setContext(({ headers = {} }) => {
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem('id_token');
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  })
-  return forward(operation);
-});
-
-const client = new ApolloClient({
-  link: concat(authLink, httpLink),
-  cache: new InMemoryCache(),
-});
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -64,10 +39,10 @@ const router = createBrowserRouter(createRoutesFromElements(
 function App() {
 
   return (
-    <ApolloProvider client={client}>
+    <>
       <GlobalStyle />
       <RouterProvider router={router} />
-    </ApolloProvider>
+    </>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = new HttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+// const authLink = new ApolloLink((operation, forward) => {
+//   operation.setContext(({ headers = {} }) => {
+//     // get the authentication token from local storage if it exists
+//     const token = localStorage.getItem('id_token');
+//     // return the headers to the context so httpLink can read them
+//     return {
+//       headers: {
+//         ...headers,
+//         authorization: token ? `Bearer ${token}` : '',
+//       },
+//     };
+//   })
+//   return forward(operation);
+// });
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ApolloProvider client={client}>
+        <App /> 
+    </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
